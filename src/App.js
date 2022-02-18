03f38c9609cf0bd2aa241c674c58b1715a46f327
unknown <lorenzo.lgayle@gmail.com>
2022-02-18 14:02:18 -0500

    getting it complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Character from './components/Character';
+import Specifics from './components/Specifics'; 
+
 import './App.css';
 
+
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  const [characterId, setCharacterId] = useState(null)
+ 
+  const openSpecifics = id => {
+    setCharacterId(id)
+  }
+  const closeSpecifics = () =>{
+    setCharacterId(null)
+  }
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then( res => {
+      console.log(res.data)
+      setCharacters(res.data)
+    })
+    .catch(err => console.error(err))
+  }, [])
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  
 
+     
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
+    <div className="Root">
       <h1 className="Header">Characters</h1>
     </div>
   );
 }
+
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,31 @@
-// Write your Character component here
+// // Write your Character component here
+// import React, { useState, useEffect} from "react";
+// import axios from 'axios'
+
+
+
+// export default function Character(props){
+//     //setting up props and state
+//     const { characterId, characters } = props
+    
+
+//     return(
+//         <div>
+//             {
+//                 characterId.map(character => {
+//                    return (<article>
+//                         <img src={character.image} alt ={character.name}></img>
+//                         <h2>Name:{character.name}</h2>
+//                         <h3>Gender:{character.gender}</h3>
+//                         <h3>Height:{character.height}</h3>
+//                         <h3>HomeWorld:{character.homeworld}</h3>
+//                         <h3>Mass:{character.mass}</h3>
+//                         <h3>Birth Year:{character.birth_year}</h3>
+//                         <h3>Number Of Films: {character.films.length}</h3>
+//                     </article>)
+//                 })
+//             }
+//         </div>
+//     )
+
+// }
